da3c7847495e55ce8066719427a1a49ab95f6b9c
Renan Assunção <rhassuncao@hotmail.com>
2019-11-21 14:59:38 -0300

    Add gl quads

--- Atividade02 - GL_Quads/main.cpp
+++ Atividade02 - GL_Quads/main.cpp
@@ -1,56 +1,81 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
-int main( void )
+#define SCREEN_WIDTH 1024
+#define SCREEN_HEIGHT 480
+
+int main(void)
 {
     GLFWwindow *window;
 
-    // Initialize the library
-    if ( !glfwInit( ) )
-    {
+    if (!glfwInit( )){
+
         return -1;
     }
 
-    // Create a windowed mode window and its OpenGL context
-    window = glfwCreateWindow( 640, 480, "Hello World", NULL, NULL );
+    window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "GL_QUADS", NULL, NULL);
 
-    if ( !window )
-    {
+    if (!window){
+
         glfwTerminate( );
         return -1;
     }
 
     // Make the window's context current
-    glfwMakeContextCurrent( window );
+    glfwMakeContextCurrent(window);
 
+    //OLD
+    /*
     float vertices[] =
     {
-        0, 0.5, 0.0, // top corner
-        -0.5, -0.5, 0.0, // bottom left corner
-        0.5, -0.5, 0.0 // bottom right corner
+        -0.5,  0.5, 0.0, // Canto superior esquerdo
+         0.5,  0.5, 0.0, // Canto superior direito
+         0.5, -0.5, 0.0, // canto inferior direito
+        -0.5, -0.5, 0.0  // Canto Inferior Direito
+    }; */
+
+    //Para Garantir que será um quadrado em qualquer tela
+    float vertices[] =
+    {
+        300, 300, 0.0, // top right corner
+          0, 300, 0.0, // top left corner
+          0,   0, 0.0, // bottom left corner
+        300,   0, 0.0 // bottom right corner
     };
 
-    /* Loop until the user closes the window */
-    while ( !glfwWindowShouldClose( window ) )
+    //Convert do espaço normalizado para o espaço de pixels
+    glViewport(0.0f, 0.0f, SCREEN_WIDTH, SCREEN_HEIGHT);
+    //Define propriedades de camera
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    //Define o sistema de coordanadas
+    glOrtho(0, SCREEN_WIDTH, 0, SCREEN_HEIGHT, 0, 1);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+    while (!glfwWindowShouldClose(window))
     {
-        glClear( GL_COLOR_BUFFER_BIT );
+        glClear(GL_COLOR_BUFFER_BIT);
 
-        // Render OpenGL here
+        glEnableClientState(GL_VERTEX_ARRAY);
+        //Aposta para os vertices que serao utilizados
+        glVertexPointer(3, GL_FLOAT, 0, vertices);
+        //size - 3 coordenadas (X,y,z)
+        //tipo - float
+        //Gaps entre vertices
+        //ponteiro para os vertices
 
-        glEnableClientState( GL_VERTEX_ARRAY ); // tell OpenGL that you're using a vertex array for fixed-function attribute
-        glVertexPointer( 3, GL_FLOAT, 0, vertices ); // point to the vertices to be used
-        glDrawArrays( GL_TRIANGLES, 0, 3 ); // draw the vertixes
-        glDisableClientState( GL_VERTEX_ARRAY ); // tell OpenGL that you're finished using the vertex arrayattribute
+        //Desenha os vertices
+        glDrawArrays(GL_QUADS, 0, 4 );
+        //Objeto a ser desenhado
+        //vertice de inicio
+        //numero de vertices
+        glDisableClientState(GL_VERTEX_ARRAY);
 
-
-        // Swap front and back buffers
         glfwSwapBuffers( window );
-
-        // Poll for and process events
         glfwPollEvents( );
     }
 
     glfwTerminate( );
-
     return 0;
 }
